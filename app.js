05ed30b4c63597fd52b0858d588d30c43825c72e
unknown <2elbesha@vsispool5.informatik.uni-hamburg.de>
2015-09-16 15:22:28 +0200

    Revamped Karma-Sys now active on index - todo on single for q's and a's

--- app.js
+++ app.js
@@ -32,6 +32,23 @@ var QuestionService= (function(){
         return question.update();
       });
     },
+    votedusers: function(id){
+      return DB.Questions.load(id).then(function(question){
+        return question.voted_users;
+      });
+    },
+    addvoteduser: function(id){
+      return DB.Questions.load(id).then(function(question){
+        question.voted_users.add(DB.User.me.username);
+        question.update();
+      });
+    },
+    initvoteduser: function(id){
+      return DB.Questions.load(id).then(function(question){
+        question.voted_users = new DB.Set([DB.User.me.username]);
+        question.update();
+      });      
+    }
   }
 })();
 
@@ -76,42 +93,82 @@ var QuestionController= (function() {
     },
     add: function(user,title, question){
       CounterService.get().then(function(count){
-      alert(count);
-      var temp_date = new Date().getTime();
-      temp_hash = HashService.createHash(title,temp_date);
-      var temp_question = new DB.Questions({
-        Q_Asker : DB.User.me.username,
-        Q_Title : title,
-        Q_Text : question,
-        Q_ID : count,
-        Q_Date : temp_date,
-        Karma : 0,
-        Q_hash : temp_hash
-      });
-      QuestionService.save(temp_question);
+        var temp_date = new Date().getTime();
+        temp_hash = HashService.createHash(title,temp_date);
+        var temp_question = new DB.Questions({
+          Q_Asker : DB.User.me.username,
+          Q_Title : title,
+          Q_Text : question,
+          Q_ID : count,
+          Q_Date : temp_date,
+          Karma : 0,
+          Q_hash : temp_hash
+        });
+        QuestionService.save(temp_question);
       });
       CounterService.increment();
-        alert('Question saved, thank you!');
+      alert('Question saved, thank you!');
       render();
     },
 
     karmaplus: function(q_id){
       QuestionService.get_real_id(q_id).then(function(id){
-        QuestionService.karmaplus(id).then(function(){
-          ctrl.showAll();
-          NewsController.onReady();
+        QuestionService.votedusers(id).then(function(result){
+          if(result != null){
+            if(result.has(DB.User.me.username))
+              alert('Sorry, you cant vote more than once for each question!');
+            else{
+              QuestionService.get_real_id(q_id).then(function(id){
+                QuestionService.karmaplus(id).then(function(){
+                  QuestionService.addvoteduser(id);
+                  ctrl.showAll();
+                  NewsController.onReady();
+                });
+              });
+            }
+          }
+          else{
+            QuestionService.initvoteduser(id);
+            QuestionService.get_real_id(q_id).then(function(id){
+              QuestionService.karmaplus(id).then(function(){
+                ctrl.showAll();
+                NewsController.onReady();
+              });
+            });
+          }
         });
-      })
+      });
     },
-      karmaminus: function(q_id){
+    karmaminus: function(q_id){
       QuestionService.get_real_id(q_id).then(function(id){
-        QuestionService.karmaminus(id).then(function(){
-          ctrl.showAll();
-          NewsController.onReady();
+        QuestionService.votedusers(id).then(function(result){
+          if(result != null){
+            if(result.has(DB.User.me.username))
+              alert('Sorry, you cant vote more than once for each question!');
+            else{
+              alert('42');
+              QuestionService.get_real_id(q_id).then(function(id){
+                QuestionService.karmaminus(id).then(function(){
+                  QuestionService.addvoteduser(id);
+                  ctrl.showAll();
+                  NewsController.onReady();
+                });
+              });
+            }
+          }
+          else{
+            QuestionService.initvoteduser(id);
+            QuestionService.get_real_id(q_id).then(function(id){
+              QuestionService.karmaminus(id).then(function(){
+                ctrl.showAll();
+                NewsController.onReady();
+              });
+            });
+          }
         });
-      })
-   }, 
-    
+
+      });
+    }, 
     onReady: function() {
       var source= $("#question_template").html();
       template = Handlebars.compile(source);
